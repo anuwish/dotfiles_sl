425f58594c4b6f78cd400be26c25eb7455c71c00
Julian Tarek Wishahi <jwishahi@lxplus0075.cern.ch>
2015-04-22 22:09:40 +0200

    Some strang things happening here.

---
+++ install.sh
@@ -0,0 +1,24 @@
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# general commands
+cd ${BASEDIR}; git submodule update --init --recursive; cd -
+${BASEDIR}/fonts/install.sh
+ln -s ${BASEDIR} ~/.dotfiles
+
+# vim
+ln -s ${BASEDIR}/vimrc ~/.vimrc
+ln -s ${BASEDIR}/vim ~/.vim
+
+# git
+ln -s ${BASEDIR}/git/gitconfig ~/.gitconfig
+cp ${BASEDIR}/git/gitignore_global ~/.gitignore_global
+
+# bash
+ln -s ${BASEDIR}/bash/bashrc         ~/.bashrc   
+ln -s ${BASEDIR}/bash/bash_profile   ~/.bash_profile 
+ln -s ${BASEDIR}/bash/bash_aliases   ~/.bash_aliases 
+ln -s ${BASEDIR}/bash/bash_exports   ~/.bash_exports 
+ln -s ${BASEDIR}/bash/bash_options   ~/.bash_options 
+ln -s ${BASEDIR}/bash/bash_paths     ~/.bash_paths   
+ln -s ${BASEDIR}/bash/bash_prompt    ~/.bash_prompt  
+ln -s ${BASEDIR}/bash/inputrc        ~/.inputrc      
